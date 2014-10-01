dfebac85d189b1bbc2bf4c3de2cad70e083c8e77
gabrieltudorica <gabriel.tudorica@ymail.com>
2014-10-01 23:22:51 +0300

    Added EmailSender

---
+++ SendingEmails/SendingEmails/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SendingEmails
+{
+    public class EmailSender
+    {
+        private readonly MailMessage _mail;
+        private readonly EmailSenderConfiguration _config;
+
+        public EmailSender(EmailSenderConfiguration config, MailMessage mail)
+        {
+            _mail = mail;
+            _config = config;
+        }
+
+        public void Send(string subject, string body)
+        {            
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Host = _config.SmtpHost;
+                smtp.Port = _config.SmtpPort;
+                smtp.Credentials = new NetworkCredential(_config.AppEmail, _config.AppEmailPassword);
+                smtp.EnableSsl = true;
+
+                smtp.Send(_mail);
+            }
+        }        
+    }
+}
---
+++ SendingEmails/SendingEmails/EmailSenderConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+
+namespace SendingEmails
+{
+    public class EmailSenderConfiguration
+    {
+        public string SmtpHost
+        {
+            get { return ConfigurationManager.AppSettings["smtpHost"]; } 
+        }
+
+        public int SmtpPort
+        {
+            get { return int.Parse(ConfigurationManager.AppSettings["smtpPort"]); }
+        }
+
+        public string AppEmail
+        {
+            get { return ConfigurationManager.AppSettings["appEmail"]; }
+        }
+
+        public string AppEmailPassword
+        {
+            get { return ConfigurationManager.AppSettings["appEmailPassword"]; }
+        }
+    }
+}
