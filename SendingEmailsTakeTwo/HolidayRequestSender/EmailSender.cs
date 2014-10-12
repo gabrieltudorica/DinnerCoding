8df42bf372e0a9c019cd82e3b479ee8e008bf84a
Gabriel Tudorica <gabriel.tudorica@ymail.com>
2014-10-12 11:55:57 +0200

    Implemented EmailSender

--- SendingEmailsTakeTwo/HolidayRequestSender/EmailSender.cs
+++ SendingEmailsTakeTwo/HolidayRequestSender/EmailSender.cs
@@ -1,11 +1,22 @@
-﻿using System.Net.Mail;
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
 
 namespace HolidayRequestSender
 {
     public class EmailSender
     {
-        public void Send(MailMessage mail)
+        public static void Send(MailMessage mail, string senderPassword)
         {
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Host = ConfigurationManager.AppSettings["smtpHost"];
+                smtp.Port = int.Parse(ConfigurationManager.AppSettings["smtpPort"]);
+                smtp.Credentials = new NetworkCredential(mail.Sender.Address, senderPassword);
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+            }
         }
     }
 }
