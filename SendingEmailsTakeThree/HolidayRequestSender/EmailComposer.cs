231282b09a700f20b56847cffe06d776448049cd
gabrieltudorica <gabriel.tudorica@ymail.com>
2014-10-16 00:27:11 +0300

    Extracted EmailComposer and created properties

---
+++ SendingEmailsTakeThree/HolidayRequestSender/EmailComposer.cs
@@ -0,0 +1,78 @@
+﻿using System.Configuration;
+using System.Net.Mail;
+
+namespace HolidayRequestSender
+{
+    public class EmailComposer
+    {
+        private readonly HolidayApplication _holidayApplication;
+       
+        public EmailComposer(HolidayApplication holidayApplication)
+        {
+            _holidayApplication = holidayApplication;
+        }
+
+        public MailMessage CreateRequestMail()
+        {
+            return new MailMessage(_holidayApplication.Employee.GetEmail(), _holidayApplication.Manager.GetEmail())
+            {
+                Subject = "Holiday Request",
+                Body = "Hi " + _holidayApplication.Manager.GetFirstName() + "," +
+                        "\n\n" +
+                        "Please approve my holiday request starting from " +
+                        _holidayApplication.HolidayInterval.GetStartDate().ToShortDateString() +
+                        " until " +
+                        _holidayApplication.HolidayInterval.GetEndDate().ToShortDateString() +
+                        "\n\n" +
+                        "Thank you!"
+            };
+        }
+
+        public MailMessage CreateApprovalMail()
+        {
+            var approvalMail = new MailMessage(_holidayApplication.Manager.GetEmail(), GetHrEmail())
+            {
+                Subject = "[Approved] Holiday Request",
+                Body = "Hi, " +
+                        "\n\n" +
+                        _holidayApplication.Employee.GetFirstName() + "'s holiday holiday request starting from " +
+                        _holidayApplication.HolidayInterval.GetStartDate().ToShortDateString() +
+                        " until " +
+                        _holidayApplication.HolidayInterval.GetEndDate().ToShortDateString() +
+                        " has been approved." +
+                        "\n\n" +
+                        "Thank you!"
+            };
+
+            approvalMail.CC.Add(_holidayApplication.Employee.GetEmail());
+
+            return approvalMail;
+        }
+
+        public MailMessage CreateRejectionMail()
+        {
+            return new MailMessage(_holidayApplication.Manager.GetEmail(), _holidayApplication.Employee.GetEmail())
+            {
+                Subject = "[Rejected] Holiday Request",
+                Body = "Hi " + _holidayApplication.Employee.GetFirstName() + "," +
+                        "\n\n" +
+                        "Unfortunately, your holiday request starting from " +
+                        _holidayApplication.HolidayInterval.GetStartDate().ToShortDateString() +
+                        " until " +
+                        _holidayApplication.HolidayInterval.GetEndDate().ToShortDateString() +
+                        " cannot be approved during this timeframe." +
+                        "\n\n" +
+                        "Thank you!"
+            };
+        }
+
+        private static MailAddress GetHrEmail()
+        {
+            return new MailAddress(
+                string.Format("{0}@{1}",
+                    ConfigurationManager.AppSettings["hrMail"],
+                    ConfigurationManager.AppSettings["companyHost"]),
+                ConfigurationManager.AppSettings["hrMail"]);
+        }
+    }
+}
--- SendingEmailsTakeThree/HolidayRequestSender/HolidayApplication.cs
+++ SendingEmailsTakeThree/HolidayRequestSender/HolidayApplication.cs
@@ -1,94 +1,16 @@
-﻿using System.Configuration;
-using System.Net.Mail;
-
-namespace HolidayRequestSender
+﻿namespace HolidayRequestSender
 {
     public class HolidayApplication
     {
-        private readonly Employee _employee;
-        private readonly Employee _manager;
-        private readonly HolidayInterval _holidayInterval;
+        public Employee Employee { get; private set; }
+        public Employee Manager { get; private set; }
+        public HolidayInterval HolidayInterval { get; private set; }
 
         public HolidayApplication(Employee employee, Employee manager, HolidayInterval holidayInterval)
         {
-            _employee = employee;
-            _manager = manager;
-            _holidayInterval = holidayInterval;
-        }
-
-        public MailMessage Create(EmailContentType mailType)
-        {
-            if (mailType == EmailContentType.Request)
-            {
-                return CreateRequestMail();
-            }
-
-            return mailType == EmailContentType.Approval ? 
-                                CreateApprovalMail() : 
-                                CreateRejectionMail();
-        }
-
-        private MailMessage CreateRequestMail()
-        {
-            return new MailMessage(_employee.GetEmail(), _manager.GetEmail())
-            {
-                Subject = "Holiday Request",
-                Body = "Hi " + _manager.GetFirstName() + "," +
-                        "\n\n" +
-                        "Please approve my holiday request starting from " +
-                        _holidayInterval.GetStartDate().ToShortDateString() +
-                        " until " +
-                        _holidayInterval.GetEndDate().ToShortDateString() +
-                        "\n\n" +
-                        "Thank you!"                    
-            };
-        }
-
-        private MailMessage CreateApprovalMail()
-        {
-            var approvalMail = new MailMessage(_manager.GetEmail(), GetHrEmail())
-            {
-                Subject = "[Approved] Holiday Request",
-                Body = "Hi, " + 
-                        "\n\n" +
-                        _employee.GetFirstName() + "'s holiday holiday request starting from " +
-                        _holidayInterval.GetStartDate().ToShortDateString() +
-                        " until " +
-                        _holidayInterval.GetEndDate().ToShortDateString() +
-                        " has been approved." +
-                        "\n\n" +
-                        "Thank you!"
-            };
-
-            approvalMail.CC.Add(_employee.GetEmail());
-            
-            return approvalMail;
-        }
-
-        private MailMessage CreateRejectionMail()
-        {
-            return new MailMessage(_manager.GetEmail(), _employee.GetEmail())
-            {
-                Subject = "[Rejected] Holiday Request",
-                Body = "Hi " + _employee.GetFirstName() + "," +
-                        "\n\n" +
-                        "Unfortunately, your holiday request starting from " +
-                        _holidayInterval.GetStartDate().ToShortDateString() +
-                        " until " +
-                        _holidayInterval.GetEndDate().ToShortDateString() +
-                        " cannot be approved during this timeframe." + 
-                        "\n\n" +
-                        "Thank you!"
-            };
-        }
-
-        private MailAddress GetHrEmail()
-        {
-            return new MailAddress(
-                string.Format("{0}@{1}",
-                    ConfigurationManager.AppSettings["hrMail"],
-                    ConfigurationManager.AppSettings["companyHost"]),
-                ConfigurationManager.AppSettings["hrMail"]);
-        }
+            Employee = employee;
+            Manager = manager;
+            HolidayInterval = holidayInterval;
+        }      
     }
 }
