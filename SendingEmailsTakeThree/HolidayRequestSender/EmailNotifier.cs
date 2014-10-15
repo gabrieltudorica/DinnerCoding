edda0d0eff37d05381c1e3ddb0d8bfa53bbc979a
gabrieltudorica <gabriel.tudorica@ymail.com>
2014-10-16 00:31:07 +0300

    Add EmailNotifier

---
+++ SendingEmailsTakeThree/HolidayRequestSender/EmailNotifier.cs
@@ -0,0 +1,23 @@
+﻿namespace HolidayRequestSender
+{
+    public class EmailNotifier : INotifier
+    {
+        public void Request(HolidayApplication holidayApplication)
+        {
+            var composer = new EmailComposer(holidayApplication);
+            EmailSender.Send(composer.CreateRequestMail());
+        }
+
+        public void Approve(HolidayApplication holidayApplication)
+        {
+            var composer = new EmailComposer(holidayApplication);
+            EmailSender.Send(composer.CreateApprovalMail());
+        }
+
+        public void Reject(HolidayApplication holidayApplication)
+        {
+            var composer = new EmailComposer(holidayApplication);
+            EmailSender.Send(composer.CreateRejectionMail());
+        }
+    }
+}
